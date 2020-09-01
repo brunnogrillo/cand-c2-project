6e095c6333ed11c3f0ef6ac0212ff34c7ed82109
brunnogrillo <brunno.grillo@hotmail.com>
2020-08-31 22:04:15 -0300

    update files

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,12 +1,15 @@
 provider "aws" {
-    access_key = "AKIATLW4KIRSOZAXUCOS"
-    secret_key = "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"
+    access_key = "AKIATLW4KIRSB7PV5LE2"
+    secret_key = "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"
     region = "us-east-1"
 }
 
 resource "aws_instance" "Udacity_T2" {
     ami = "ami-0c6b1d09930fac512"
     instance_type = "t2.micro"
+    tags = {
+        Name = "Udacity T2"
+    }
     count = 4
     subnet_id = "subnet-2272d244"
 }
