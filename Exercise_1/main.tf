cc3334ff353e6147ffb8b0efb5334e8473a4ce16
brunnogrillo <brunno.grillo@hotmail.com>
2020-09-01 08:11:55 -0300

    update main

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -13,3 +13,13 @@ resource "aws_instance" "Udacity_T2" {
     count = 4
     subnet_id = "subnet-2272d244"
 }
+
+resource "aws_instance" "Udacity_M4" {
+    ami = "ami-0c6b1d09930fac512"
+    instance_type = "m4.large"
+    tags = {
+        Name = "Udacity M4"
+    }
+    count = 2
+    subnet_id = "subnet-2272d244"
+}
